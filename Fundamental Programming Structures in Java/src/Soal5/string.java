a6463289efb614cfdea26e5bde5b768d3eff888f
Rifkiyusran <rifki.yusran.tif20@polban.ac.id>
2021-09-24 06:47:39 +0700

    update soal 3, 4 dan 5

---
+++ Fundamental Programming Structures in Java/src/Soal5/string.java
@@ -0,0 +1,30 @@
+package Soal5;
+
+
+import java.util.Scanner;
+public class string {
+	public static void main(String[] args) {
+		String A, B;
+		int length,i;
+		try (Scanner keyboard = new Scanner(System.in)) {
+			System.out.println("Masukan kata  :");
+			A = keyboard.nextLine();
+			System.out.println("Masukkan Kata :");
+			B = keyboard.nextLine();
+		}
+		length = A.length() + B.length();
+		System.out.println("Jumlah Kata A dan B adalah :" +length);
+
+		i = A.compareTo(B);
+		if(i>0) {
+			System.out.println("Yes");
+		}
+		else {
+			System.out.println("No");
+		}
+		System.out.printf(A.substring(0,1).toUpperCase()+A.substring(1, A.length()));
+        System.out.println(" " + B.substring(0,1).toUpperCase()+B.substring(1, B.length()));
+		
+	}
+
+}
