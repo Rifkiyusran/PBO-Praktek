a410aea6e9b0e2a52cdffac4ee0845b6d04692fe
Rifkiyusran <rifki.yusran.tif20@polban.ac.id>
2021-11-20 09:42:07 +0700

    kasus W9

---
+++ W9_Polymorphism Dan Interface/src/Kasus1/Paint.java
@@ -0,0 +1,18 @@
+package Kasus1;
+
+public class Paint {
+	private double coverage; // number of square feet per gallon
+	
+	public Paint(double c) { // constructor
+		coverage = c;
+	}
+	
+	public double amount(Shape s) { 
+		// return the amount of paint (number of gallons)
+		// needed to paint the shape given as the parameter
+		System.out.println("Computing amount for " + s);
+		return s.area()/coverage; // corrected
+		// before:
+		// return 0;
+	}
+}
